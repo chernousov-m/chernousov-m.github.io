3e30deaf91c97cf7a1dfb7d085c9be5515977a1c
Maxim Chernousov <50081610+chernousov-m@users.noreply.github.com>
2024-05-06 01:13:52 +0400

    google search

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -29,6 +29,7 @@ export default (() => {
           </>
         )}
         <meta name="google-site-verification" content="PbloHGAr3asPvrqJnClGOjRHSf2LmKjuMNZoCi5nXP4" />
+        <meta name="google-site-verification" content="ciRgxDK7SUIaSQgeq9fcyFrFfXl0G1OJyyp6K3ZJd5g" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
